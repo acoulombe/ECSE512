8fab29f0ac2faf58473d822ee01bae9ec7257288
Alexandre Coulombe <alexcoulombe98@gmail.com>
2019-10-28 14:48:20 -0400

    LMS algorithm turned into function

--- test_LMS.m
+++ test_LMS.m
@@ -1,32 +1,26 @@
-M = 100;                    % Controls how narrow the pass-band gets
-nD = 100;                   % Makes the system uncorrelated with itself
-samples = 10000000;
+M = 50;                    % Controls how narrow the pass-band gets
+nD = 50;                   % Makes the system uncorrelated with itself
+samples = 1000;
 n = 1:samples;
-freq=0.2;
-i = 5*sin(2*pi*freq*n);   % narrowband signal
+freq=0.125;
+i = 2*sin(2*pi*freq*n);   % narrowband signal
 
 s = randn(size(i));         % wideband signal
 x = s+i;                    % input signal
-mu =0.0000001;              % Learning rate/step-size of the convergence to the optimal system
+mu =0.0001;              % Learning rate/step-size of the convergence to the optimal system
 d = x;                      % Desired signal
 
-h = zeros(1,M);             % System function
-e = zeros(1,samples);       % Error signal
-y = zeros(1,samples);       % Output estimate signal
+[y,e,h] = LMS(x,x,M,nD, mu);
 
-for j = nD+M:samples 
-    y(j) = x(j-M-nD+1:j-nD)*h';
-    e(j) = d(j)-y(j);
-    h = h + mu*e(j)*x(j-M-nD+1:j-nD);
-end
+fft_samples = 500;
+x_fft = abs(fft(x(samples-fft_samples:samples)));
+y_fft = abs(fft(y(samples-fft_samples:samples)));
+e_fft = abs(fft(e(samples-fft_samples:samples)));
+h_fft = abs(fft(h,fft_samples+1));
 
-x_fft = abs(fft(x(samples-1000:samples)));
-y_fft = abs(fft(y(samples-1000:samples)));
-e_fft = abs(fft(e(samples-1000:samples)));
-h_fft = abs(fft(flip(h),1001));
-
-subplot(5,1,1); plot(x); title('input signal');
-subplot(5,1,2); plot(y); title('estimated interference signal');
-subplot(5,1,3); plot(h_fft); title('FFT of system');
-subplot(5,1,4); plot(y_fft); title('FFT of estimated Interference');
-subplot(5,1,5); plot(e_fft); title('FFT of output signal');
+subplot(6,1,1); plot(x); title('input signal');
+subplot(6,1,2); plot(y); title('estimated interference signal');
+subplot(6,1,3); plot((s-e).^2); title('error signal');
+subplot(6,1,4); plot(0:2/fft_samples:2,h_fft); title('FFT of system'); xlabel('angular frequency (pi rad/s)');
+subplot(6,1,5); plot(0:2/fft_samples:2,y_fft); title('FFT of estimated Interference'); xlabel('angular frequency (pi rad/s)');
+subplot(6,1,6); plot(0:2/fft_samples:2,e_fft); title('FFT of output signal'); xlabel('angular frequency (pi rad/s)');
